a2329d730270dfea12225179dd3c89f19ad4f325
Sabrina Simkhovich <sabrinasimkhovich@gmail.com>
2025-05-24 23:55:35 -0500

    added point functions and history functions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
 #include "external/tinyxml2/tinyxml2.h"
 
 using namespace tinyxml2;
@@ -48,16 +49,60 @@ vector<GPSPoint> parseGPX(const string& filename) {
     return points;
 }
 
-int main() {
-    string filename = "ride.gpx";  // Make sure this file exists
-    vector<GPSPoint> ride = parseGPX(filename);
+void saveHistory(const vector<GPSPoint>& points, const string& filename){
+    // file pointer
+    fstream fout;
 
-    for (const auto& pt : ride) {
-        cout << "Lat: " << pt.latitude
-             << ", Lon: " << pt.longitude
-             << ", Elev: " << pt.elevation
-             << ", Time: " << pt.timestamp << endl;
+    // opens an existing csv file or creates a new file.
+    fout.open(filename, ios::out | ios::app);
+
+    //write point info to file
+    for (const auto& pt : points) {
+        fout << pt.latitude << ", "
+             << pt.longitude << ", "
+             << pt.elevation << ", "
+             << pt.timestamp<< ", "
+             << "\n";
     }
+
+}
+
+vector<GPSPoint> loadHistory(const string& filename){
+    //create new point vector for ride
+    vector<GPSPoint> ride = parseGPX(filename);
+    return ride;
+}
+
+//haversine function to calculate distance between two points
+double haversine(double lat1, double lon1, double lat2, double lon2){
+
+}
+
+bool isPointExplored(const GPSPoint& pt, const vector<GPSPoint>& history){
+//load points from history into vector (using loadHistory)
+//compare point to points in history
+//use haversine function 
+//return 0 if not explored , 1 if explored
+}
+
+
+
+
+int main() {
+    string rideName = "ride.gpx";  // Make sure this file exists
+    //create points vector from parsing ride data
+    vector<GPSPoint> ride = parseGPX(rideName);
+
+    // history file name
+    string filename = "history.csv"; 
+
+    saveHistory(ride, filename);
+    // for (const auto& pt : ride) {
+    //     cout << "Lat: " << pt.latitude
+    //          << ", Lon: " << pt.longitude
+    //          << ", Elev: " << pt.elevation
+    //          << ", Time: " << pt.timestamp << endl;
+    // }
 
     return 0;
 }
